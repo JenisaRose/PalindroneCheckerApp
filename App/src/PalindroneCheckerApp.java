4cca9df07ed75802612a4b7625adbf047cb370b9
JenisaRose <rohini.pal009@gmail.com>
2026-02-27 16:13:25 +0530

    JenisaRose Added UC1 - Application Entry & Welcome Message

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,4 +1,4 @@
-public class PalindromeCheckerApp {
+public class PalindroneCheckerApp {
 
     // Application constants
     private static final String APP_NAME = "PalindromeChecker App";
@@ -8,9 +8,21 @@ public class PalindromeCheckerApp {
 
         displayWelcomeMessage();
 
-        // Future feature:
-        // Continue to next use case (e.g., prompt user for input)
-        // or exit application
+        // Hardcoded string
+        String testString = "madam";
+
+        // Check palindrome
+        boolean isPalindrome = checkPalindrome(testString);
+
+        // Display result
+        System.out.println("Testing String: " + testString);
+        if (isPalindrome) {
+            System.out.println("Result: It is a palindrome.");
+        } else {
+            System.out.println("Result: It is NOT a palindrome.");
+        }
+
+        System.out.println("Application exiting...");
     }
 
     private static void displayWelcomeMessage() {
@@ -18,5 +30,15 @@ public class PalindromeCheckerApp {
         System.out.println("Welcome to " + APP_NAME);
         System.out.println("Version: " + VERSION);
         System.out.println("===================================");
+    }
+
+    private static boolean checkPalindrome(String input) {
+        String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        return input.equals(reversed);
     }
 }
